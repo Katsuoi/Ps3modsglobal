98c12aa2fa126c5c2d8f27a6b7f7245acb6cc9b6
Katsuoi <oonputy@gmail.com>
2026-07-24 17:19:40 -0300

    Create mods.js

---
+++ mods.js
@@ -0,0 +1,24 @@
+const mods = [
+
+{
+    id: 1,
+
+    nome: "Follower Light And Shade",
+
+    categoria: "Companions",
+
+    imagem: "../imagens/mod1.jpg",
+
+    descricao: "Adicione uma nova seguidora no Bannered Mare em Whiterun chamada Eris (Light Shade), para acompanhá-lo em suas aventuras.",
+
+    download: "https://www.mediafire.com/file/gw9ssk3ib6shs2i/Light+And+Shade.zip/file",
+
+    galeria: [
+        "../imagens/Follower Light And Shade/foto1.jpg",
+        "../imagens/Follower Light And Shade/foto2.jpg",
+        "../imagens/Follower Light And Shade/foto3.jpg"
+    ]
+
+}
+
+];
