47d1612ce9b1353873b745412d66edc7fdf0d534
Katsuoi <oonputy@gmail.com>
2026-07-27 02:15:22 -0300

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,19 +1,60 @@
 const mods = [
+
+
 {
     id: 1,
+
     nome: "Follower Light And Shade",
+
     categoria: "Companions",
+
     autor: "Convertido por PS3ModsGlobal",
-    versao: "1.0",
+
+    versao: "n/a",
+
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-    data: "2026",
+
+    data: "27/7/26",
+
     imagem: "../imagens/mod1.jpg",
-    descricao: "Adicione uma nova seguidora no Bannered Mare em Whiterun chamada Eris (Light Shade), para acompanhá-lo em suas aventuras.",
+
+    descricao: "Adicione uma nova seguidora no Bannered Mare em Whiterun chamada Eris (Light Shade).",
+
     download: "https://www.mediafire.com/file/gw9ssk3ib6shs2i/Light+And+Shade.zip/file",
+
     galeria: [
         "../imagens/follower light and shade/foto1.jpg",
         "../imagens/follower light and shade/foto2.jpg",
         "../imagens/follower light and shade/foto3.jpg"
     ]
-}
-];
+},
+
+
+{
+    id: 2,
+
+    nome: "YuiH Standalone Follower",
+
+    categoria: "Companions",
+
+    autor: "yui0820 (convertido por PS3ModsGlobal)",
+
+    versao: "1.1",
+
+    compatibilidade: "Skyrim Legendary Edition (PS3)",
+
+    data: "27/7/26",
+
+    imagem: "../imagens/mod2.jpg",
+
+    descricao: "Adiciona Sha, uma seguidora personalizada. Localização: Riverwood, Sleeping Giant Inn.",
+
+    download: "https://www.mediafire.com/file/010wvpidoqpkm59/YuiH_StandAlone_Follower_-_Sha_v1_1_%2528Renewal%2529_UNP.zip/file",
+
+    galeria: [
+        "../imagens/YuiH-Standalone-Follower/foto1.jpg",
+        "../imagens/YuiH-Standalone-Follower/foto2.jpg"
+    ]
+},
+
+    
