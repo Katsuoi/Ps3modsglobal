17b98f786f64dd6b16094f8fdcbd6a793bb1c8e7
Katsuoyy <oonputy@gmail.com>
2026-07-28 13:39:27 -0300

    Update mods.js

--- mods.js
+++ mods.js
@@ -2,31 +2,18 @@ const mods = [
 
 {
     id: 1,
-
     nome: "Follower Light And Shade",
-
     categoria: "Companions",
-
     autor: "Craftias (convertido por: N/A)",
-
     versao: "N/A",
-
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-
     data: "27/07/2026",
-
     casamento: "Sim",
-
     localizacao: "Bannered Mare - Whiterun",
-
     classe: "Guerreira",
-
     imagem: "../imagens/mod1.jpg",
-
     descricao: "Adicione uma nova seguidora no Bannered Mare em Whiterun chamada Eris (Light Shade).",
-
     download: "https://www.mediafire.com/file/gw9ssk3ib6shs2i/Light+And+Shade.zip/file",
-
     galeria: [
         "../imagens/follower light and shade/foto1.jpg",
         "../imagens/follower light and shade/foto2.jpg",
@@ -36,31 +23,18 @@ const mods = [
 
 {
     id: 2,
-
     nome: "YuiH Standalone Follower - Sha",
-
     categoria: "Companions",
-
     autor: "yui0820 (convertido por PS3ModsGlobal)",
-
     versao: "1.1",
-
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-
     data: "27/07/2026",
-
     casamento: "Não",
-
     localizacao: "Sleeping Giant Inn - Riverwood",
-
     classe: "Guerreira",
-
     imagem: "../imagens/mod2.jpg",
-
     descricao: "Adiciona Sha, uma seguidora personalizada localizada na Sleeping Giant Inn em Riverwood.",
-
     download: "https://www.mediafire.com/file/010wvpidoqpkm59/YuiH_StandAlone_Follower_-_Sha_v1_1_%2528Renewal%2529_UNP.zip/file",
-
     galeria: [
         "../imagens/YuiH-Standalone-Follower/foto1.jpg",
         "../imagens/YuiH-Standalone-Follower/foto2.jpg",
@@ -70,31 +44,18 @@ const mods = [
 
 {
     id: 3,
-
     nome: "Teyla - Nord Half-Elf",
-
     categoria: "Companions",
-
     autor: "vsmods97 (convertido por: N/A)",
-
     versao: "N/A",
-
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-
     data: "27/07/2026",
-
     casamento: "Sim",
-
     localizacao: "The Arcanaeum - College of Winterhold",
-
     classe: "Maga",
-
     imagem: "../imagens/mod3.jpg",
-
     descricao: "Conheça Misty, uma linda meio-elfa Nord e maga estudiosa do Colégio de Winterhold.",
-
     download: "https://www.mediafire.com/file/jpz1ed368o7qlq8/Teyla+Elf.zip/file",
-
     galeria: [
         "../imagens/Teyla-Nord-Half-Elf/foto1.jpg",
         "../imagens/Teyla-Nord-Half-Elf/foto2.jpg",
@@ -104,31 +65,18 @@ const mods = [
 
 {
     id: 4,
-
     nome: "Overknee Rebecca Follower",
-
     categoria: "Companions",
-
     autor: "Overknee (convertido por: N/A)",
-
     versao: "0.9",
-
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-
     data: "27/07/2026",
-
-    casamento: "",
-
-    localizacao: "",
-
-    classe: "",
-
+    casamento: "N/A",
+    localizacao: "N/A",
+    classe: "N/A",
     imagem: "../imagens/mod4.jpg",
-
     descricao: "Apresentamos Rebecca, uma seguidora elegante e letal, com um visual marcante. Perfeita para quem quer estilo e poder ao lado durante as aventuras em Skyrim.",
-
     download: "https://www.mediafire.com/file/3dy8ix0wv7n6akk/Rebeca.7z/file",
-
     galeria: [
         "../imagens/Overknee-Rebecca-Follower/foto1.jpg",
         "../imagens/Overknee-Rebecca-Follower/foto2.jpg",
@@ -138,31 +86,18 @@ const mods = [
 
 {
     id: 5,
-
     nome: "Myrcella Standalone Healer Follower",
-
     categoria: "Companions",
-
-    autor: "Dovahkiinathay (convertido por: ps3modsglobal",
-
+    autor: "Dovahkiinathay (convertido por PS3ModsGlobal)",
     versao: "2.0",
-
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-
     data: "27/07/2026",
-
-    casamento: "não",
-
-    localizacao: "Templo de Kynareth, em Whiterun.",
-
+    casamento: "Não",
+    localizacao: "Templo de Kynareth - Whiterun",
     classe: "Maga de Restauração",
-
     imagem: "../imagens/mod5.jpg",
-
     descricao: "Uma curandeira experiente e companheira leal, dedicada a proteger seus aliados com poderosas magias de Restauração.",
-
     download: "https://www.mediafire.com/file/ztr7im4bkmbng65/Myrcella+Follower+-+CBBE++(1).zip/file",
-
     galeria: [
         "../imagens/Myrcella-Standalone-Healer-Follower/foto1.jpg",
         "../imagens/Myrcella-Standalone-Healer-Follower/foto2.jpg",
@@ -172,31 +107,18 @@ const mods = [
 
 {
     id: 6,
-
     nome: "Lili Follower",
-
     categoria: "Companions",
-
-    autor: "overknee(convertido por: ps3modsglobal",
-
+    autor: "Overknee (convertido por PS3ModsGlobal)",
     versao: "1.3",
-
     compatibilidade: "Skyrim Legendary Edition (PS3)",
-
     data: "27/07/2026",
-
-    casamento: "não",
-
-    localizacao: "Quicksilver mine ou Windpeak inn",
-
-    classe: "Maga de destruição",
-
+    casamento: "Não",
+    localizacao: "Quicksilver Mine ou Windpeak Inn",
+    classe: "Maga de Destruição",
     imagem: "../imagens/mod6.jpg",
-
     descricao: "Conheça Lili, uma seguidora fofa e leal, pronta para acompanhar você em qualquer jornada por Skyrim.",
-
     download: "https://www.mediafire.com/file/5k8k9qyh7sw0fgf/LiliFollowerVer(YoungEager).zip/file",
-
     galeria: [
         "../imagens/Lili-Follower/foto1.jpg",
         "../imagens/Lili-Follower/foto2.jpg",
@@ -205,4 +127,3 @@ const mods = [
 }
 
 ];
-
